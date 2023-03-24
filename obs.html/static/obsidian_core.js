b215649e85d10f46478e0e090b3d40648843d9e9
patrickdown <patrick.down@gmail.com>
2023-03-24 12:00:13 -0400

    Fix link

--- obs.html/static/obsidian_core.js
+++ obs.html/static/obsidian_core.js
@@ -10,7 +10,7 @@ var CONFIGURED_HTML_URL_PREFIX = "";
 var RELATIVE_PATHS = 0;
 var documentation_mode = 1;
 var tab_mode = !no_tab_mode;
-var gzip_hash = '21835091656635534043098542572792599895'                       // used to check whether the localStorage data is stale
+var gzip_hash = '149766180525846681634726341314092079008'                       // used to check whether the localStorage data is stale
 
 // global cache
 var fn_cache_ls_available = null;
