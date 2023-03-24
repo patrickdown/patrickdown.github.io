7697a6b967e2cd0ef29e2e6e5edbddc04193ce06
patrickdown <patrick.down@gmail.com>
2023-03-24 11:01:01 -0400

    Update documentation

--- obs.html/static/obsidian_core.js
+++ obs.html/static/obsidian_core.js
@@ -11,7 +11,7 @@ var CONFIGURED_HTML_URL_PREFIX = "";
 var RELATIVE_PATHS = 0;
 var documentation_mode = 1;
 var tab_mode = !no_tab_mode;
-var gzip_hash = '34784641766400140896737822111875707750'                       // used to check whether the localStorage data is stale
+var gzip_hash = '165807037045983496215398096199160056855'                       // used to check whether the localStorage data is stale
 
 
 // Onloads
